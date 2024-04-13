164a8fd36dee96e29e2982bce0e2cf22a6463ae3
s31ma <sdogana000@citymail.cuny.edu>
2024-04-13 19:29:44 -0400

    initial commit

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Import your models after initializing SQLAlchemy
+from model import Event, Calendar
+
+# Create the database tables within the context of the Flask application
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/events")
+def  events():
+    return ""
